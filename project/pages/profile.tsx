0f335941ba5526967d8e2f318f8e63796f303c49
gani <agani2032@gmail.com>
2023-10-30 01:28:19 +0300

    feat: complete /

--- project/pages/profile.tsx
+++ project/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import avatar from '../assets/img/avatar.png'
 import { IProducts } from '../util/typesObj';
 import { getFavProduct } from '../services/productsData';
